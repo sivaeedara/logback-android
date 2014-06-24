fd87995b201c26dba9544da3e846148a31b2047a
Sebastian Gröbler <s.groebler@googlemail.com>
2014-06-24 15:24:14 +0800

    :seedling: added missing unit test for re-establishing the socket connection when stream creation fails with IOException

--- logback-core/src/test/java/ch/qos/logback/core/net/AbstractSocketAppenderTest.java
+++ logback-core/src/test/java/ch/qos/logback/core/net/AbstractSocketAppenderTest.java
@@ -382,7 +382,7 @@ public class AbstractSocketAppenderTest {
   }
 
   @Test
-  public void reEstablishesSocketConnectionOnConnectionDrop() throws Exception {
+  public void reEstablishesSocketConnectionOnConnectionDropWhenWritingEvent() throws Exception {
 
     // given
     doThrow(new IOException()).when(objectWriter).write(anyObject());
@@ -394,6 +394,20 @@ public class AbstractSocketAppenderTest {
 
     // then
     verify(objectWriterFactory, timeout(TIMEOUT).atLeast(2)).newAutoFlushingObjectWriter(any(OutputStream.class));
+  }
+
+  @Test
+  public void triesToReEstablishSocketConnectionIfItFailed() throws Exception {
+
+    // given
+    doThrow(new IOException()).when(socket).getOutputStream();
+    appender.start();
+
+    // when
+    appender.append("some event");
+
+    // then
+    verify(socketConnector, timeout(TIMEOUT).atLeast(2)).call();
   }
 
   @Test
